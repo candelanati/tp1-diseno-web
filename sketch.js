a6e2e42ac526cdb4e1dd4daddd991358f8b3f4a5
candelanati <candelanati23@gmail.com>
2025-09-17 21:56:29 -0300

    funcionando arriba de todo / sin estilar

---
+++ sketch.js
@@ -0,0 +1,10 @@
+let miCanvas
+function setup() {
+    miCanvas = createCanvas(visualViewport, windowHeight);
+    miCanvas.parent('padre-script'); 
+    
+}
+
+function draw() {
+    background("#d9d9d9");
+}
